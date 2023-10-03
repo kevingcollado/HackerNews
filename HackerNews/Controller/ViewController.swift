b1c50f5ee543a3d5c01a0a8187fbe769cdfe908c
Kevin Gustavo Collado <kcolladojimenez@gmail.com>
2023-10-02 22:59:28 -0600

    Actualiza la tabla con los datos de la API
    Actualiza la tabla con los datos de la API

--- HackerNews/Controller/ViewController.swift
+++ HackerNews/Controller/ViewController.swift
@@ -10,11 +10,9 @@ import Alamofire
 
 class ViewController: UIViewController{
     //Instancia de NetworkManager
-    let networkManager = NetworkManager()
     var resultAPIData = [Post]()
     //Outlets
     @IBOutlet weak var mainTableView: UITableView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,34 +21,38 @@ class ViewController: UIViewController{
         mainTableView.delegate = self
         //Registrar celda personalizada
         mainTableView.register(UINib(nibName: "CustomCell", bundle: nil), forCellReuseIdentifier: "CustomCell")
-        //Llama al método getPost() para obtener los datos de la API
-        networkManager.getPost { apiData in
-            self.resultAPIData = apiData.hits
-        }
-        //Actualiza la tabla con los datos de la API
-        DispatchQueue.main.async {
-            self.mainTableView.reloadData()
-        }
         
+        //Llama al método getPost() para obtener los datos de la API
+        NetworkManager.sharedInstanced.getPost { apiData in
+            self.resultAPIData = apiData.hits
+            //Actualiza la tabla con los datos de la API
+            DispatchQueue.main.async {
+                self.mainTableView.reloadData()
+            }
+        }
     }
 }
+
+
+
+
+
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(String(describing: resultAPIData))
         return resultAPIData.count
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
-    }
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return UITableView.automaticDimension
+        }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as? CustomCell
-        cell?.titleLabel.text = resultAPIData[indexPath.row].author
+        cell?.titleLabel.text = resultAPIData[indexPath.row].title
         cell?.authorLabel.text = resultAPIData[indexPath.row].author
-        cell?.pointsLabel.text = resultAPIData[indexPath.row].points.description
+        cell?.pointsLabel.text = resultAPIData[indexPath.row].points?.description
         cell?.createdAtLabel.text = resultAPIData[indexPath.row].created_at
         return cell!
     }
--- HackerNews/Model/NetworkManager.swift
+++ HackerNews/Model/NetworkManager.swift
@@ -10,41 +10,52 @@ import Alamofire
 
 
 final class NetworkManager {
+    static let sharedInstanced = NetworkManager()
+    
     func getPost(handler: @escaping(_ apiData: Results)->(Void)) {
-        AF.request("https://hn.algolia.com/api/v1/search?tags=front_page").response { response in
+        let url = "https://hn.algolia.com/api/v1/search?tags=front_page"
+        
+        AF.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil, interceptor: nil).response { response in
             switch response.result {
             case .success(let data):
                 do {
                     let jsonData = try JSONDecoder().decode(Results.self, from: data!)
                     handler(jsonData)
-                } catch {
-                    print(error)
+                    print(jsonData)
+                }catch{
+                    print(String(describing: error))
                 }
             case .failure(let error):
-                print(error)
+                print(String(describing: error))
             }
         }
     }
 }
-
-
-//    if let url = URL(string: url){
-//        let session = URLSession(configuration: .default)
-//        let task = session.dataTask(with: url) { (data, response, error) in
-//            if error == nil{
-//                let decoder = JSONDecoder()
-//                if let safeData = data{
-//                    do{
-//                        let results = try decoder.decode(Results.self, from: safeData)
-//                        print(results.hits)
-//                    }catch{
-//                        print("No se han encontrado entradas \(error.localizedDescription)")
-//                    }
-//                }
-//            }
-//        }
-//        task.resume()
-//    }
-//}
-
-
+        
+        //final class NetworkManager {
+        //    static let sharedInstanced = NetworkManager()
+        //    private let url = "https://hn.algolia.com/api/v1/search?tags=front_page"
+        //
+        //    func getPost(success: @escaping(_ apiData: Results)->(), failure: @escaping (_ error: Error?)->()){
+        //        AF.request(url, method: .get).response { response in
+        //            guard let data = response.data else {
+        //                print("Error getting data from API")
+        //                return
+        //            }
+        //
+        //            switch response.result {
+        //            case .success:
+        //                do {
+        //                    let jsonData = try JSONDecoder().decode(Results.self, from: data)
+        //                    success(jsonData)
+        //                } catch {
+        //                    print(error)
+        //                }
+        //            case .failure(let error):
+        //                print(error)
+        //            }
+        //
+        //        }
+        //    }
+        //}
+        
